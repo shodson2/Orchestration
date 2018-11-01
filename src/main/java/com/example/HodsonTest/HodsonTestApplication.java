d74aefc019afc09dbe02a72369a90da7e5759f50
shodson2 <shodson2@yahoo.com>
2018-10-31 20:07:18 -0500

    Initial Add

---
+++ src/main/java/com/example/HodsonTest/HodsonTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.HodsonTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HodsonTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HodsonTestApplication.class, args);
+	}
+}
